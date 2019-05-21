753b586ef86f62d107ead1177e85998707600943
haluskua <haluskua@users.noreply.github.com>
2019-05-22 02:28:15 +1200

    sprint 4 added, still incomplete challenge

--- script.js
+++ script.js
@@ -14,3 +14,23 @@ document.getElementById("menu-button", "sprint1-drop").onclick = function() {myF
 function myFunction() {
   document.getElementById("myDropdown", "myDropdown2").classList.toggle("show");
 }  
+
+
+// var result = function sum(a, b){
+//   var actual = a + b;
+// }
+
+function testSum (a, b ) {
+  var expected = 7;
+  actual = (4 + 3);
+
+  if (actual != expected) {
+      console.log("It's broken..")
+  } else {
+      console.log("It works!")
+  }
+}
+  // could call the funtion 'sum' or the variable 'result'
+var result = function sum(){
+  return testSum();
+};
